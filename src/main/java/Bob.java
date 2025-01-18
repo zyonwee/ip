5f8649eb2df80372bb07b5ea6d7066d029a53169
zyonwee <zyonwee@gmail.com>
2025-01-18 18:15:46 +0800

    Added echoMsg method

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bob {
 
 
@@ -6,8 +8,28 @@ public class Bob {
             "What can I do for you? \n" ;
     static String exitMsg =  "Bye. Hope to see you again soon!\n";
 
+    /**
+     * OnStart will display greeting message
+     * Thereafter, echo user's input
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println(greetMsg);
-        System.out.println(exitMsg);
+        boolean shouldEcho = true;
+        while (shouldEcho) {
+            echoMsg();
+        }
     }
+
+    /**
+     * Echoes the User's Message
+     */
+    public static void echoMsg(){
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        String echo = sc.nextLine();  // Read user
+        System.out.println("Are you sure you want me to " + echo + "? " +
+                "\nWhy can't you do it yourself? :3");
+    }
+
+
 }
