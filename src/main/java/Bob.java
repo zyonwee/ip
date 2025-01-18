96f2dd78ec6d897f5a95341a028965910d1a5c0f
zyonwee <zyonwee@gmail.com>
2025-01-18 21:15:19 +0800

    Added List Commands

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -9,7 +9,7 @@ public class Bob {
     static String greetMsg = "Hello! I'm " + fullName + "\n" +
             "What can I do for you? \n" ; // Greeting Message
     static String exitMsg =  "Bye. Please don't trouble me again!\n"; // Exit Message
-    static boolean echoMode = true; // Check if Bot should continue to echo
+    static boolean listenMode = true; // Check if Bot should continue to listen for commands
     static List<String> commandList = new ArrayList<String>(); // List of user inputs
 
     /**
@@ -27,30 +27,49 @@ public class Bob {
      * Handles the user input
      */
     public static void handleCommand(){
-        while (echoMode) {
+        while (listenMode) {
             Scanner sc = new Scanner(System.in);  // Create a Scanner object
             String command = sc.nextLine();  // Read user input
             // On exit command
             if (command.equals("bye")){
-                echoMode = false;
-            } else {
-
+                listenMode = false;
+            }
+            // On list command
+            else if (command.equals("list")){
+                listCommands();
+            }
+            // Echos Command
+            else {
                 echoMsg(command);
             }
-
 
         }
     }
 
     /**
      * Echoes the User's Message
-     * @return boolean indicates if bot should exit echoMode
      */
-    public static void echoMsg(String echo){
+    public static void echoMsg(String command){
+        // Add the command to the list of Commands
+        commandList.add(command);
         // Echo
-        System.out.println("Are you sure you want me to " + echo + "? " +
-                "\nWhy can't you do it yourself? :3");
+        System.out.println("Are you sure you want me to " + command + "? " +
+                "\nWhy can't you do it yourself? :3\n");
      }
+
+
+    /**
+     * List our the history of User's Commands
+     */
+    public static void listCommands(){
+        System.out.println("Woah woah woah, \nlet me get this straight... You want me to :\n");
+        int index = 0; // Index of Command
+        for (String s : commandList) {
+            System.out.println(Integer.toString(index) + ". " + s);
+            index += 1;
+        }
+        System.out.println("\nI think you're asking for way to much >.<\n");
+    }
 
 
 }
