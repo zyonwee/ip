8ea17ba591a8c4fc417770d449e5edbbf8735049
zyonwee <zyonwee@gmail.com>
2025-01-18 10:47:07 +0800

    Add exit message

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -4,8 +4,10 @@ public class Bob {
     static String fullName = "Bob the Princess";
     static String greetMsg = "Hello! I'm " + fullName + "\n" +
             "What can I do for you? \n" ;
+    static String exitMsg =  "Bye. Hope to see you again soon!\n";
 
     public static void main(String[] args) {
-        System.out.println(greetMsg); 
+        System.out.println(greetMsg);
+        System.out.println(exitMsg);
     }
 }
